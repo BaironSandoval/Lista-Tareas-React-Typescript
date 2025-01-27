00aa05a4d0d52ed994952c686d7ed2e1e4e6e698
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2025-01-27 18:27:21 -0500

    proyecto terminado

---
+++ src/components/ListaTareas.tsx
@@ -0,0 +1,20 @@
+import { Tarea } from "./Tarea";
+
+type ListaTareas = {
+  listaTareas: string[];
+  borrarTareas: (index: number) => void;
+};
+
+export const ListaTareas = ({ listaTareas, borrarTareas }: ListaTareas) => {
+  return (
+    <div className="taskList">
+      {listaTareas.map((tarea, index) => (
+        <Tarea
+          key={index}
+          tarea={tarea}
+          borrarTarea={() => borrarTareas(index)}
+        ></Tarea>
+      ))}
+    </div>
+  );
+};
